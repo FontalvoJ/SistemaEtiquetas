9c4385102354889d5ab875696d5ddbd46dbec553
José David F <90539362+FontalvoJ@users.noreply.github.com>
2024-11-06 09:27:48 -0500

    Nuevos ajustes

--- Imprimir.Designer.cs
+++ Imprimir.Designer.cs
@@ -45,9 +45,9 @@
             // btnImprimir
             // 
             this.btnImprimir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(53)))), ((int)(((byte)(77)))));
-            this.btnImprimir.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnImprimir.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnImprimir.Location = new System.Drawing.Point(328, 452);
+            this.btnImprimir.Location = new System.Drawing.Point(328, 501);
             this.btnImprimir.Name = "btnImprimir";
             this.btnImprimir.Size = new System.Drawing.Size(187, 61);
             this.btnImprimir.TabIndex = 0;
@@ -58,79 +58,79 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(241, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.Size = new System.Drawing.Size(161, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Productor";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(240, 190);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 205);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 27);
+            this.label2.Size = new System.Drawing.Size(123, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "Helado";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(241, 278);
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(241, 301);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 27);
+            this.label3.Size = new System.Drawing.Size(119, 37);
             this.label3.TabIndex = 3;
             this.label3.Text = "Envase";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(241, 366);
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(241, 397);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 27);
+            this.label4.Size = new System.Drawing.Size(86, 37);
             this.label4.TabIndex = 4;
             this.label4.Text = "Peso";
             // 
             // cbxProductorEti
             // 
-            this.cbxProductorEti.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxProductorEti.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxProductorEti.FormattingEnabled = true;
-            this.cbxProductorEti.Location = new System.Drawing.Point(245, 143);
+            this.cbxProductorEti.Location = new System.Drawing.Point(245, 153);
             this.cbxProductorEti.Name = "cbxProductorEti";
-            this.cbxProductorEti.Size = new System.Drawing.Size(339, 35);
+            this.cbxProductorEti.Size = new System.Drawing.Size(339, 45);
             this.cbxProductorEti.TabIndex = 5;
             this.cbxProductorEti.SelectedIndexChanged += new System.EventHandler(this.cbxProductorEti_SelectedIndexChanged);
             // 
             // cbxHeladoEti
             // 
-            this.cbxHeladoEti.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxHeladoEti.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxHeladoEti.FormattingEnabled = true;
-            this.cbxHeladoEti.Location = new System.Drawing.Point(245, 225);
+            this.cbxHeladoEti.Location = new System.Drawing.Point(245, 249);
             this.cbxHeladoEti.Name = "cbxHeladoEti";
-            this.cbxHeladoEti.Size = new System.Drawing.Size(339, 35);
+            this.cbxHeladoEti.Size = new System.Drawing.Size(339, 45);
             this.cbxHeladoEti.TabIndex = 6;
             this.cbxHeladoEti.SelectedIndexChanged += new System.EventHandler(this.cbxHeladoEti_SelectedIndexChanged);
             // 
             // cbxEnvaseEti
             // 
-            this.cbxEnvaseEti.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxEnvaseEti.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbxEnvaseEti.FormattingEnabled = true;
-            this.cbxEnvaseEti.Location = new System.Drawing.Point(245, 321);
+            this.cbxEnvaseEti.Location = new System.Drawing.Point(245, 345);
             this.cbxEnvaseEti.Name = "cbxEnvaseEti";
-            this.cbxEnvaseEti.Size = new System.Drawing.Size(339, 35);
+            this.cbxEnvaseEti.Size = new System.Drawing.Size(339, 45);
             this.cbxEnvaseEti.TabIndex = 7;
             this.cbxEnvaseEti.SelectedIndexChanged += new System.EventHandler(this.cbxEnvaseEti_SelectedIndexChanged);
             // 
             // txtPesoEti
             // 
-            this.txtPesoEti.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPesoEti.Location = new System.Drawing.Point(245, 404);
+            this.txtPesoEti.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesoEti.Location = new System.Drawing.Point(247, 441);
             this.txtPesoEti.Name = "txtPesoEti";
-            this.txtPesoEti.Size = new System.Drawing.Size(339, 36);
+            this.txtPesoEti.Size = new System.Drawing.Size(339, 44);
             this.txtPesoEti.TabIndex = 8;
             this.txtPesoEti.TextChanged += new System.EventHandler(this.txtPesoEti_TextChanged);
             // 
@@ -148,9 +148,9 @@
             // btnRegresar
             // 
             this.btnRegresar.BackColor = System.Drawing.Color.Maroon;
-            this.btnRegresar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegresar.ForeColor = System.Drawing.Color.Snow;
-            this.btnRegresar.Location = new System.Drawing.Point(328, 519);
+            this.btnRegresar.Location = new System.Drawing.Point(328, 577);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(187, 65);
             this.btnRegresar.TabIndex = 13;
@@ -162,7 +162,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 596);
+            this.ClientSize = new System.Drawing.Size(869, 654);
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtPesoEti);
